c1e6d0cf62ba45fb37644575594b8321871db008
imranpopz <imranpopz32@gmail.com>
2020-09-11 08:20:17 +0000

    remove vendorsetup.sh

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,5 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_a6000.mk
+
+COMMON_LUNCH_CHOICES := omni_a6000-eng
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo omni_a6000-eng
-add_lunch_combo omni_a6000-userdebug
